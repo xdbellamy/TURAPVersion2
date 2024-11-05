09218451f93dd42ec29d14dd2dd0e333b4d29823
Aske Kruse <kruseaske@gmail.com>
2024-11-05 15:07:36 +0100

    New weapon = weapon3

--- BestGameEver/Program.cs
+++ BestGameEver/Program.cs
@@ -8,6 +8,7 @@
             HP_Potion potion = new HP_Potion(1, "Health Potion", 50, 1, 1);
             Weapon weapon1 = new Weapon(1, "Enormous Bazooka", "Bazooka", 50, 60, 100);
             Weapon weapon2 = new Weapon(2, "Small Shotgun", "Shotgun", 30, 50, 100);
+            Weapon weapon3 = new Weapon(3, "Dretdretdret", "Sub-Machinegun", 30, 50, 100);
             Player playerOne = new Player(1, "Volodymyr Zelenskyj", 100, 0, weapon1, potion);
             Enemy enemy = new Enemy(1, "Vladimir Putin", 100, 1, weapon2, 10);
 
